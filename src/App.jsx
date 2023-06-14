bb25e880a840ba2b31802ac134828100ebda82f7
KarineDHoshi <karinedhoshi@yahoo.com>
2023-06-14 16:33:05 +0200

    initial login signup

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useEffect } from 'react';
+import { useAtom } from 'jotai';
+import { userAtom } from './userAtom';
+import Register from './pages/register';
+import Sign_in from './pages/login';
+
+
+import Cookies from 'js-cookie';
+
+function App() {
+  //const [user] = useAtom(userAtom);
+  const [, setUser] = useAtom(userAtom);
+
+  useEffect(() => {
+    const token = Cookies.get('token');
+    const id = Cookies.get('id');
+
+    if (token) {
+      setUser({
+        id: id,
+        isLoggedIn: true,
+        token: token,
+      });
+    }
+  }, []);
+
+  return (
+ 
+        <div>
+        <Register />
+        <Sign_in />
+      </div>
+       
+      )}
+
+
+export default App;
+
