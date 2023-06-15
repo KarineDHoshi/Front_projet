51fbbbc06138da39ffa7879855dd43673c6ae517
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-06-15 16:55:35 +0200

    LD redirect when connected

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import Sign_in from "../components/logIn/loginForm";
+import Profile from './Profile';
+import { useAtom } from 'jotai';
+import { userAtom } from '../atoms/userAtom';
 
 const LogIn = () => {
+  const [user] = useAtom(userAtom);
 
   return (
-    <Sign_in />
+    <>
+      {user.isLoggedIn ? (
+        <Profile />
+      ) : (
+        <Sign_in />
+      )}
+    </>
   );
 }
 
