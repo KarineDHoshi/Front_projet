808147dbdda4eec7e3292ad36313678d120fa4c5
KarineDHoshi <karinedhoshi@yahoo.com>
2023-06-15 13:54:47 +0200

    navbar css still working on

--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -3,43 +3,46 @@ import { Link } from "react-router-dom";
 import { useAtom } from "jotai";
 import { userAtom } from "../../atoms/userAtom";
 import Logout from "../LogOut/logoutButton";
+import './navbar.css'; // Import the CSS file for navbar styles
 
 const Navbar = () => {
   const [user] = useAtom(userAtom);
 
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Accueil</Link>
-        </li>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <ul className="navbar-list">
+          <li className="navbar-item">
+            <Link to="/">Accueil</Link>
+          </li>
+          {user.isLoggedIn ? (
+            <>
+              <li className="navbar-item">
+                <Link to="/profile">Profil</Link>
+              </li>
+            </>
+          ) : (
+            <>
+              <li className="navbar-item">
+                <Link to="/users">Inscription</Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/users/sign_in">Se Connecter</Link>
+              </li>
+            </>
+          )}
+        </ul>
         {user.isLoggedIn ? (
           <>
-            <li>
-              <Link to="/profile">Profil</Link>
-            </li>
+            <div>
+              <p> Bienvenue !</p>
+            </div>
+            <div>
+              <Logout />
+            </div>
           </>
-        ) : (
-          <>
-            <li>
-              <Link to="/users">Inscription</Link>
-            </li>
-            <li>
-              <Link to="/users/sign_in">Se Connecter</Link>
-            </li>
-          </>
-        )}
-      </ul>
-      {user.isLoggedIn ? (
-        <>
-          <div>
-            <p> Bienvenue !</p>
-          </div>
-          <div>
-            <Logout />
-          </div>
-        </>
-      ) : null}
+        ) : null}
+      </div>
     </nav>
   );
 };
